bfbc53925b74a6b391d66442c4376437594e13cf
sali <sali2@loyalty.com>
2016-03-19 08:07:58 -0400

    rearranging

--- src/main/java/com/alphasystem/openxml/builder/wml/HeadingList.java
+++ src/main/java/com/alphasystem/openxml/builder/wml/HeadingList.java
@@ -5,50 +5,6 @@ package com.alphasystem.openxml.builder.wml;
  */
 public abstract class HeadingList extends AbstractListItem<HeadingList> {
 
-    public static final HeadingList HEADING5 = new HeadingList("ListHeading5") {
-        @Override
-        public HeadingList getNext() {
-            return null;
-        }
-
-        @Override
-        public String getName() {
-            return "HEADING5";
-        }
-    };
-    public static final HeadingList HEADING4 = new HeadingList("ListHeading4") {
-        @Override
-        public HeadingList getNext() {
-            return HEADING5;
-        }
-
-        @Override
-        public String getName() {
-            return "HEADING4";
-        }
-    };
-    public static final HeadingList HEADING3 = new HeadingList("ListHeading3") {
-        @Override
-        public HeadingList getNext() {
-            return HEADING4;
-        }
-
-        @Override
-        public String getName() {
-            return "HEADING3";
-        }
-    };
-    public static final HeadingList HEADING2 = new HeadingList("ListHeading2") {
-        @Override
-        public HeadingList getNext() {
-            return HEADING3;
-        }
-
-        @Override
-        public String getName() {
-            return "HEADING2";
-        }
-    };
     public static final HeadingList HEADING1 = new HeadingList(11, "ListHeading1", "04090021") {
         @Override
         public HeadingList getNext() {
@@ -60,6 +16,56 @@ public abstract class HeadingList extends AbstractListItem<HeadingList> {
             return "HEADING1";
         }
     };
+
+    public static final HeadingList HEADING2 = new HeadingList("ListHeading2") {
+        @Override
+        public HeadingList getNext() {
+            return HEADING3;
+        }
+
+        @Override
+        public String getName() {
+            return "HEADING2";
+        }
+    };
+
+    public static final HeadingList HEADING3 = new HeadingList("ListHeading3") {
+        @Override
+        public HeadingList getNext() {
+            return HEADING4;
+        }
+
+        @Override
+        public String getName() {
+            return "HEADING3";
+        }
+    };
+
+    public static final HeadingList HEADING4 = new HeadingList("ListHeading4") {
+        @Override
+        public HeadingList getNext() {
+            return HEADING5;
+        }
+
+        @Override
+        public String getName() {
+            return "HEADING4";
+        }
+    };
+
+    public static final HeadingList HEADING5 = new HeadingList("ListHeading5") {
+        @Override
+        public HeadingList getNext() {
+            return null;
+        }
+
+        @Override
+        public String getName() {
+            return "HEADING5";
+        }
+    };
+
+
     private static final int LEFT_INDENT_VALUE = 432;
     private static final int INCREMENT_VALUE = 144;
 
