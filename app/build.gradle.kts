2f55609dddb0c265b113b9f4494304d73fd0256d
sfali23 <f.syed.ali@gmail.com>
2026-05-15 12:19:23 -0400

    Refactor version management: migrate to `libs.versions.toml` and update build scripts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,32 +10,20 @@ repositories {
     mavenCentral()
 }
 
-val asCommonsVersion: String by project
-val docx4jVersion: String by project
-val moxyVersion: String by project
-val eclipseMoxyVersion: String by project
-val slf4jApiVersion: String by project
-val logbackClassicVersion: String by project
-val testngVersion: String by project
-val guiceVersion: String by project
-val reportngVersion: String by project
-val commonsCodecVersion: String by project
-val batikVersion: String by project
-val guavaVersion: String by project
-
 dependencies {
-    api("io.github.sfali23:commons:$asCommonsVersion")
-    api("org.docx4j:docx4j-core:$docx4jVersion")
-    api("org.docx4j:docx4j-export-fo:$docx4jVersion")
-    api("org.docx4j:docx4j-JAXB-MOXy:$docx4jVersion")
-    api("org.docx4j:docx4j-MOXy-JAXBContext:$moxyVersion")
-    api("org.eclipse.persistence:org.eclipse.persistence.moxy:$eclipseMoxyVersion")
-    api("org.docx4j:docx4j-JAXB-ReferenceImpl:$docx4jVersion")
-    api("org.slf4j:slf4j-api:$slf4jApiVersion")
-    api("ch.qos.logback:logback-classic:$logbackClassicVersion")
-    testImplementation("org.testng:testng:$testngVersion")
-    testImplementation("com.google.inject:guice:$guiceVersion")
-    testImplementation("org.uncommons:reportng:$reportngVersion")
+    api("io.github.sfali23:commons:${libs.versions.asCommons.get()}")
+    api("org.docx4j:docx4j-core:${libs.versions.docx4j.get()}")
+    api("org.docx4j:docx4j-export-fo:${libs.versions.docx4j.get()}")
+    api("org.docx4j:docx4j-JAXB-MOXy:${libs.versions.docx4j.get()}")
+    api("org.docx4j:docx4j-JAXB-ReferenceImpl:${libs.versions.docx4j.get()}")
+    api("org.docx4j:docx4j-MOXy-JAXBContext:${libs.versions.moxy.get()}")
+    api("org.eclipse.persistence:org.eclipse.persistence.moxy:${libs.versions.eclipseMoxy.get()}")
+    api("org.slf4j:slf4j-api:${libs.versions.slf4jApi.get()}")
+    api("ch.qos.logback:logback-classic:${libs.versions.logbackClassic.get()}")
+    testImplementation("org.testng:testng:${libs.versions.testng.get()}")
+    testImplementation("com.google.inject:guice:${libs.versions.guice.get()}")
+    testImplementation("com.google.guava:guava:${libs.versions.guava.get()}")
+    testImplementation("org.uncommons:reportng:${libs.versions.reportng.get()}")
 }
 
 group = "io.github.sfali23"
@@ -74,7 +62,7 @@ mavenPublishing {
     publishToMavenCentral(automaticRelease = true)
     signAllPublications()
 
-    coordinates("io.github.sfali23", "docx4j-builder", "$version")
+    coordinates("io.github.sfali23", "docx4j-builder", libs.versions.libVersion.get())
 
     pom {
         name.set("Open XML Builder")
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,10 +6,8 @@ repositories {
     }
 }
 
-val docx4jVersion: String by project
-
 dependencies {
-    implementation("com.sun.codemodel:codemodel:2.6")
-    implementation("org.docx4j:docx4j-core:11.5.13")
-    implementation("org.apache.commons:commons-text:1.15.0")
+    implementation("com.sun.codemodel:codemodel:${libs.versions.codemodel.get()}")
+    implementation("org.docx4j:docx4j-core:${libs.versions.docx4j.get()}")
+    implementation("org.apache.commons:commons-text:${libs.versions.commonsText.get()}")
 }
